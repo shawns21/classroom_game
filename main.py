05934310f7347efc7ff869389950fe5a18e25bbf
jsin <jsinlinux@gmail.com>
2018-05-08 00:28:07 -0400

    change map filename

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ class Game:
 
     def load_data(self):
         game_folder = path.dirname(__file__)
-        self.map = Map(path.join(game_folder, 'map2.txt'))
+        self.map = Map(path.join(game_folder, 'map.txt'))
 
     def new(self):
         # initialize all variables and do all the setup for a new game
